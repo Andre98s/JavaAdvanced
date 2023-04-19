bcd0d6bb827344b9a8b0e8254963c9a0bc48449d
Andre98s <126459071+Andre98s@users.noreply.github.com>
2023-04-19 11:15:54 +0200

    Add files via upload
    Exercise - Records, ternary operator, BigInteger

---
+++ _11/Person.java
@@ -0,0 +1,14 @@
+package advancedjava._11;
+
+public record Person(String name, int age, String address) {
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
+
