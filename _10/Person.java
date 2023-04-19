df153ad4ba1fb6bc07384c83c70e295844651163
Andre98s <126459071+Andre98s@users.noreply.github.com>
2023-04-19 11:15:11 +0200

    Add files via upload
    Exercise - Object.class, Method overriding, equals, toString

---
+++ _10/Person.java
@@ -0,0 +1,54 @@
+package advancedjava._10;
+
+import java.util.Objects;
+
+public class Person {
+    String name;
+    int age;
+    String address;
+
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\n"+" name = '" + name + '\'' +
+                "\n"+" age = " + age +
+                "\n"+" address = '" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age + address.hashCode();
+    }
+
+}
